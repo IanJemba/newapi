0da3eba3071560364d41b7040eef5fdbea7bbc15
IanJemba <ianjemba38@gmail.com>
2024-09-09 15:28:32 +0200

    table made

--- database/migrations/2024_09_09_124116_create_vehicles_table.php
+++ database/migrations/2024_09_09_124116_create_vehicles_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('vehicles', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+
+            $table->foreignId('user_id')->constrained();
+            $table->string('plate_number');
+            $table->softDeletes();
+
         });
     }
 
