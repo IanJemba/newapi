2b46d99b06157ac3d2e3dbc6420ea684dabea007
IanJemba <ianjemba38@gmail.com>
2024-09-16 14:05:12 +0200

    done

--- app/Http/Resources/ParkingResource.php
+++ app/Http/Resources/ParkingResource.php
@@ -14,7 +14,6 @@ class ParkingResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
 
         return [
             'user_id'    => $this->user_id,
